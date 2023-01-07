ccda6dedc8a7eab791c9f4b6c61eefcdde426cdf
mechnotech <eashumilov@fintechiq.ru>
2023-01-07 16:49:27 +0300

    ts - valid view

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import numpy as np
 from dash import Dash, html, dcc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
-from helpers import get_prepared_data
+from helpers import get_prepared_data, isolation_forest
 
 app = Dash(__name__)
 
@@ -9,11 +11,22 @@ app = Dash(__name__)
 # see https://plotly.com/python/px-arguments/ for more options
 df = get_prepared_data()
 df['avg'] = df.mean(axis=1)
-df['dash_date'] = df.index
+coll = '176.59.128.0-176.59.159.255'
+
+def check_anomaly(ip_network: str):
+    result = isolation_forest(data=df, coll=ip_network, cont=0.02)
+    df['anomaly'] = result
+    df['to_show'] = df.apply(lambda x: x[ip_network] if x['anomaly'] == -1 else None, axis=1)
 
 
-
-fig = px.line(df, x=df.index, y='avg')
+check_anomaly(coll)
+fig = px.line(
+    df,
+    x=df.index,
+    y=coll,
+    # range_x=['2021-12-01', '2022-02-15']
+)
+fig.add_trace(go.Scatter(mode="markers", x=df.index, y=df['to_show'], name="Anomaly"))
 
 app.layout = html.Div(children=[
     html.H1(children='Аномалии'),
--- helpers.py
+++ helpers.py
@@ -15,3 +15,21 @@ def get_prepared_data() -> pd.DataFrame:
     grouper = df.groupby([pd.Grouper(freq='1H'), 'inetnum'])
     df = grouper['cnt'].count().unstack('inetnum').fillna(0)
     return df.resample('1H').sum()
+
+
+def scale_ip_data(data: pd.Series) -> pd.DataFrame:
+    scaler = StandardScaler()
+    np_scaled = scaler.fit_transform(data.values.reshape(-1,1))
+    return pd.DataFrame(np_scaled)
+
+
+def isolation_forest(data: pd.DataFrame, coll: str, cont: float = 0.05) -> np.array:
+    data = scale_ip_data(data[coll])
+    model = IsolationForest(contamination=cont)
+    model.fit(data)
+    return model.predict(data)
+
+def create_iso_forest_chart(data: pd.DataFrame, coll: str, cont_factor: float = 0.05):
+    result = isolation_forest(data=data, coll=coll, cont=cont_factor)
+    data['anomaly'] = result
+    return data
